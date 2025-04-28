bfa3a0a154344a1853a539cb18aa56fda884cf67
UIJIN0602 <jinkongjoo@naver.com>
2025-04-28 16:28:25 +0900

    앱 - 대리점 메인화면(80%) 앱 파일명 수정

---
+++ OrderNet-APP/app/src/main/java/bitc/fullstack/app/Branch/BranchMainActivity.kt
@@ -0,0 +1,21 @@
+package bitc.fullstack.app.Branch
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import bitc.fullstack.app.R
+
+class BranchMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_branch_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
