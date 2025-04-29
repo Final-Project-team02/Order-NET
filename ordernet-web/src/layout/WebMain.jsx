952c258e3c319f0b6bec5600fdcfade3c845b854
Yun6126 <daholam1578@naver.com>
2025-04-29 09:36:09 +0900

    대리점 메인 화면

--- ordernet-web/src/layout/WebMain.jsx
+++ ordernet-web/src/layout/WebMain.jsx
@@ -1,5 +1,6 @@
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
 import HQMain from "../HQ/HQMain.jsx";
+import BranchMain from "../Client/BranchMain.jsx";
 
 function WebMain() {
   return (
@@ -7,10 +8,10 @@ function WebMain() {
         <Routes>
           {/* 처음 '/'로 들어오면 '/HQMain'으로 리디렉션 */}
           <Route path="/" element={<Navigate to="/HQMain" replace />} />
-
           <Route path={'/HQMain'}>
             <Route index element={<HQMain />} />
           </Route>
+          <Route path="/BranchMain" element={<BranchMain/>} />
         </Routes>
       </BrowserRouter>
   );
