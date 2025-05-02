38c37de2a0d0db199701b5f6f369e02afcc83c1f
DESKTOP-GJFL5LP\it <ggiu0455@naver.com>
2025-05-02 17:26:38 +0900

    물류센터 입고 재고 조회 출력 완성

---
+++ OrderNet-Server/src/main/java/bitc/fullstack503/ordernetserver/controller/WHController.java
@@ -0,0 +1,40 @@
+package bitc.fullstack503.ordernetserver.controller;
+
+import bitc.fullstack503.ordernetserver.dto.WHDTO;
+import bitc.fullstack503.ordernetserver.service.WHService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequestMapping("/WHMain")
+@RestController
+public class WHController {
+
+
+    @Autowired
+    private WHService whService;
+
+    //    물류센터 재고조회
+    @GetMapping("")
+    public Map<String, List<WHDTO>> selectWHcheck() {
+        // 입고 리스트
+        List<WHDTO> comeInList = whService.selectWHComeIn();
+
+        //  재고 리스트
+        List<WHDTO> stockList = whService.selectWHStock();
+
+        Map<String, List<WHDTO>> result = new HashMap<>();
+        result.put("comeInList", comeInList);
+        result.put("stockList", stockList);
+
+        return result;
+    }
+
+
+}
